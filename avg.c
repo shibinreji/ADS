63d447ee38fb503cc2d88bace7554ebb17ea0706
shibinreji <117815019+shibinreji@users.noreply.github.com>
2022-11-09 15:00:04 +0530

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+ int a,b;
+ printf("Enter two numbers \n");
+ scanf("%d \n %d",&a,&b);
+ printf("avg=%d\n",(a+b)/2);
+}
---
+++ evenodd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+ int num;
+ printf("Enter the number \n");
+ scanf("%d",&num);
+if (num == 0)
+  {
+   printf("Even\n");
+  }
+else if (num % 2 == 0)
+{
+ printf("Zero\n");
+}
+else
+{
+  printf("Odd\n");
+}
+
+}
